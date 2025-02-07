89f69cd131f731464ac32e0640f430fdd98f0ed6
Jorik Mantana <bicksonboy@gmail.com>
2025-02-07 18:22:45 +0400

    Orders api Add added

--- Orders.API/obj/Debug/net8.0/Orders.API.AssemblyInfo.cs
+++ Orders.API/obj/Debug/net8.0/Orders.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orders.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd2150f726c19a9cf4a650aa39f90ef1db7dda7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9879129e64bbfa647da86aee24f77edc94bbb1e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orders.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orders.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
