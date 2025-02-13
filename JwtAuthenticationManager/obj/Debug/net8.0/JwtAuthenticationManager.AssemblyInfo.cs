af2497668cd0920fa0e01520a48904dfcb552d63
Jorik Mantana <bicksonboy@gmail.com>
2025-02-13 17:03:58 +0400

    Created BigInteger converter

--- JwtAuthenticationManager/obj/Debug/net8.0/JwtAuthenticationManager.AssemblyInfo.cs
+++ JwtAuthenticationManager/obj/Debug/net8.0/JwtAuthenticationManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtAuthenticationManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6788d9ec4bc48d374d1d98b466c70a27ba0b53d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4188d502de12b0813bc98af7aa0e2c057513318a")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtAuthenticationManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtAuthenticationManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
